8ca0d0217d06ecc84fdeb522e8213eb62baac30a
Ivy <100193316+ivcvy@users.noreply.github.com>
2022-03-18 17:56:28 +0800

    Update config.jsx

--- src/App/config.jsx
+++ src/App/config.jsx
@@ -2,34 +2,34 @@ import { GitHub, LinkedIn, Resume, Email } from 'icons';
 
 const config = {
   name: {
-    display: 'Adam Alston',
-    aria: 'My name is Adam Alston',
+    display: 'Ivy Cerelia Valerie',
+    aria: '',
   },
   title: {
-    display: 'Software Engineer',
-    aria: 'I am a software engineer',
+    display: '',
+    aria: '',
   },
   buttons: [
     {
-      href: 'https://github.com/adamalston/',
+      href: 'https://github.com/ivcvy',
       aria: 'Visit my GitHub profile',
       icon: <GitHub />,
       label: 'GitHub',
     },
     {
-      href: 'https://www.linkedin.com/in/adam-alston/',
+      href: 'https://www.linkedin.com/in/ivycv/',
       aria: 'Visit my LinkedIn profile',
       icon: <LinkedIn />,
       label: 'LinkedIn',
     },
     {
-      href: 'https://drive.google.com/drive/folders/10k8NWflSYQ5laPzuWtK3bzUKzuOeas8i/',
+      href: 'https://drive.google.com/file/d/1I2ix58uc_xJlmKUHBy7EE-W7gDyt5D0s/view?usp=sharing',
       aria: 'View my resume in Google Drive',
       icon: <Resume />,
       label: 'Resume',
     },
     {
-      href: 'mailto:aalston9@gmail.com',
+      href: 'mailto:cevalierre@gmail.com',
       aria: 'Send me an email',
       icon: <Email />,
       label: 'Email',
